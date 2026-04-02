1932fb217dcde17da70b5a32734b4ab118aad01f
future-html Frontend developer <115347865+future-html@users.noreply.github.com>
2026-04-02 10:25:12 +0700

    feat: understanding flask with mongodb

--- backend/app.py
+++ backend/app.py
@@ -19,56 +19,38 @@ client = MongoClient(mongo_uri, tlsCAFile=certifi.where())
 
 try:
     client.admin.command('ping')
-    print("✅ Successfully connected to MongoDB!")
+    print("✅ Successfully connected to Mongodb_users!")
 except ConnectionFailure:
-    print("❌ Failed to connect to MongoDB. Check your connection string.")
+    print("❌ Failed to connect to Mongodb_users. Check your connection string.")
 # 2. Select the database and collection
-db = client['sample_mflix']
-users_collection = db['users']
+db_users = client['kanban']
+users_collection = db_users['user']
 
 # --- ROUTES ---
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    """Retrieve all users from the database."""
-    try:
-        users = []
-        # Find all documents in the collection
-        for user in users_collection.find():
-            users.append({
-                "id": str(user['_id']), 
-                "name": user.get('name', 'No Name Provided'), 
-                "email": user.get('email', 'No Email Provided')
-            })
-            
-        return jsonify(users), 200
-    except Exception as e:
-        # Added error handling just in case the query fails
-        return jsonify({"error": str(e)}), 500
+@app.route('/users/<user_id>', methods = ['GET'])
+def get_user_by_id(user_id):
+    print(user_id, 'user_id')
+    query = {"_id": ObjectId(user_id)}
+    result = users_collection.find_one(query)
+    result['_id'] = str(result['_id'])
+    return jsonify({"message": f"User ID received: {user_id}", "data": result}), 200
+
 
 
 @app.route('/users', methods=['POST'])
-def create_user():
-    """Create a new user in the database."""
-    data = request.get_json()
-    
-    if not data or 'name' not in data or 'email' not in data:
-        return jsonify({"error": "Missing name or email"}), 400
+def create_new_user(): 
+    data = request.get_json(); 
+    print(data['email']); 
+    users_collection.insert_one(data)
+    print(data);
+    return jsonify({"message": "User created successfully"}), 201 
 
-    new_user = {
-        "name": data['name'],
-        "email": data['email']
-    }
-    
-    # Insert the document into MongoDB
-    try:
-        result = users_collection.insert_one(new_user)
-        return jsonify({
-            "message": "User created successfully", 
-            "id": str(result.inserted_id)
-        }), 201
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+# if use <user_id> it will get user_id in parameter function
+# def get_user_by_id(user_id): ==> like this
+
+# if the query parameter ==> /users?userId=123
+# it uses request.args.get('userId')
 
 
 if __name__ == '__main__':
